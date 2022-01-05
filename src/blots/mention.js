f3dec488cafa263d11958edba7958ff443c63e41
Al <alfredo.sirait@nttindonesia.com>
2022-01-05 14:55:16 +0700

    custom mention.js

--- src/blots/mention.js
+++ src/blots/mention.js
@@ -1,32 +1,16 @@
-import Quill from "quill";
+import Quill from 'quill';
 
-const Embed = Quill.import("blots/embed");
+const Embed = Quill.import('blots/embed');
+
 
 class MentionBlot extends Embed {
   static create(data) {
     const node = super.create();
-    // prefix character
-    const denotationChar = document.createElement("span");
-    denotationChar.className = "ql-mention-denotation-char";
+    const denotationChar = document.createElement('span');
+    denotationChar.className = 'ql-mention-denotation-char';
     denotationChar.innerHTML = data.denotationChar;
-    denotationChar.setAttribute("contenteditable", false);
-
-    // Content
-    const dataContainer = document.createElement("span");
-    dataContainer.innerHTML = data.value;
-    dataContainer.setAttribute("contenteditable", false);
-
-    // when android keyboard reaches a `contenteditable=false` block, it automatically closes.
-    // avoid that by adding a buffer "space" without the attribute.
-    const AndroidBackspaceFix = document.createElement("span");
-    AndroidBackspaceFix.innerHTML = "&nbsp;";
-    // it needs to be "visible" in order to work - so limit to minimal size.
-    AndroidBackspaceFix.setAttribute("style", "display: inline-block; height: 1px; width: 1px; overflow: hidden; ");
-
     node.appendChild(denotationChar);
-    node.appendChild(dataContainer);
-    node.appendChild(AndroidBackspaceFix);
-
+    node.innerHTML += data.value;
     return MentionBlot.setDataValues(node, data);
   }
 
@@ -50,23 +34,23 @@ class MentionBlot extends Embed {
 
   // android Gboard backspace does not fire onkeypress events, resulting in the caret
   // breaking into the read-only blot element. - so we need to handle edit events inside the blot child elements as well
-  update(mutations, context) {
-    // `childList` mutations are not handled on Quill
-    // see `update` implementation on:
-    // https://github.com/quilljs/quill/blob/master/blots/embed.js
-
-    // any attempt at modifying the inner content will just remove it
-    // (since we cant block any modifiications completely, this is the "lesser evil" / graceful fallback)
-    for (const mutation of mutations) {
-      if (mutation.type === "attributes" && mutation.attributeName === "contenteditable") continue;
-      setTimeout(() => this.remove(), 0);
-      return;
-    }
-  }
+  // update(mutations, context) {
+  //   // `childList` mutations are not handled on Quill
+  //   // see `update` implementation on:
+  //   // https://github.com/quilljs/quill/blob/master/blots/embed.js
+  //
+  //   // any attempt at modifying the inner content will just remove it
+  //   // (since we cant block any modifiications completely, this is the "lesser evil" / graceful fallback)
+  //   for (const mutation of mutations) {
+  //     if (mutation.type === "attributes" && mutation.attributeName === "contenteditable") continue;
+  //     setTimeout(() => this.remove(), 0);
+  //     return;
+  //   }
+  // }
 }
 
-MentionBlot.blotName = "mention";
-MentionBlot.tagName = "span";
-MentionBlot.className = "mention";
+MentionBlot.blotName = 'mention';
+MentionBlot.tagName = 'span';
+MentionBlot.className = 'mention';
 
 Quill.register(MentionBlot);
